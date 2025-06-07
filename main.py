61ec245e09a468e70810155827f713181b69c4ca
Sergey Mochalov <proweb@users.noreply.github.com>
2025-06-08 01:17:49 +0300

    Update app

--- main.py
+++ main.py
@@ -9,33 +9,50 @@ st.set_page_config(
     initial_sidebar_state="expanded",
     menu_items={'About': "Пример приложения от Сереги"}
 )
+st.markdown("""
+            # TODO 
+            
+            Храним данные в сессии в виде List
+            
+            """)
 
-# Инициализация списка сообщений
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+# Session storage
+# if "item" not in st.session_state:
+#     st.session_state.item = ''
+
+if "item" not in st.session_state:
+    st.session_state.item = []
 
 
 # Отображение всех сообщений
-st.title("Задачки на сегодня:")
-for msg in st.session_state.messages:
-    st.write(f"**{msg['name']}:** {msg['message']}")
-    st.divider()
+# st.caption("Session state:")
+# st.write(st.session_state)
 
+ 
+st.subheader('Вывод ДО формы')
+st.caption("Хранилище сессии")
+st.write(st.session_state)
 
-st.write("---")
 
 # Заголовок приложения
-st.header("Что сделать хозяин надо?")
+st.subheader("Форма для добавления?")
 
-# Форма для ввода данных
-with st.form("guestbook_form"):
-    name = st.text_input("Задача")
-    message = st.text_area("Примечание")
-    submitted = st.form_submit_button("Старт")
+# Пример формы
+with st.form("todo_form", clear_on_submit=True):
+    input = st.text_input("Задача на сегодня:", icon=":material/arrow_forward:")
+    submitted = st.form_submit_button("Submit", type="primary")
 
-    if submitted and name and message:
-        # Добавление нового сообщения
-        st.session_state.messages.append({"name": name, "message": message})
+    if submitted and input:
+        # Заменит значение
+        st.session_state.item.append(input)
+        st.rerun()
 
 
 
+st.subheader('Вывод после формы')
+st.caption("То что ввели")
+st.write(input)
+st.caption("st.session_state.item")
+st.write(st.session_state)
+st.caption("Отправлена форма или нет")
+st.write(submitted)
