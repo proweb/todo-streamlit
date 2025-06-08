e709faea1828fe69b48f01dd150c00ce8ec64d58
Sergey Mochalov <proweb@users.noreply.github.com>
2025-06-08 08:41:13 +0300

    Update code with sidebar example

--- main.py
+++ main.py
@@ -7,26 +7,19 @@ st.set_page_config(
     page_icon=":tomato:",
     layout="centered",
     initial_sidebar_state="expanded",
-    menu_items={'About': "Пример приложения от Сереги"}
 )
-st.markdown("""
-            # TODO 
-            
-            Храним данные в сессии в виде List
-            
-            """)
 
-# Session storage
-# if "item" not in st.session_state:
-#     st.session_state.item = ''
+st.sidebar.write("""
+# TODO :tomato:
+            
+Храним данные в сессии в виде List
+""" )
+
 
 if "item" not in st.session_state:
     st.session_state.item = []
 
 
-# Отображение всех сообщений
-# st.caption("Session state:")
-# st.write(st.session_state)
 
  
 st.subheader('Вывод ДО формы')
@@ -38,8 +31,8 @@ st.write(st.session_state)
 st.subheader("Форма для добавления?")
 
 # Пример формы
-with st.form("todo_form", clear_on_submit=True):
-    input = st.text_input("Задача на сегодня:", icon=None)
+with st.form("formTodo", clear_on_submit=True):
+    input = st.text_input(label="Задача на сегодня:", placeholder='Что надо сделать?')
     submitted = st.form_submit_button("Submit", type="primary")
 
     if submitted and input:
